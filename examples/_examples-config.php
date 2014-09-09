eb20588d6e2f21299e66bdccaa6a255cc48719f6
Jasmine Hegman <jasmine@webpt.com>
2014-09-09 15:34:50 -0700

    Altering config to point to devdruid

--- examples/_examples-config.php
+++ examples/_examples-config.php
@@ -1,7 +1,10 @@
 <?php
 
 return array(
-    'druid-host' => '10.20.30.119',
-    'druid-port' => '9001',
-    'druid-dataSource' => 'referral-visit-test-data',
+//    'druid-host' => '10.20.30.119',
+//    'druid-port' => '9001',
+//    'druid-dataSource' => 'referral-visit-test-data',
+    'druid-host' => 'devdruid.webpt.com',
+    'druid-port' => '8080',
+    'druid-dataSource' => 'referral-visit-test-data-w-facility-ids'
 );
