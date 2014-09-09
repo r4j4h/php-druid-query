a87f52e7918678f342280da83cfae61b2303cf02
Jasmine Hegman <jasmine@webpt.com>
2014-09-09 00:42:29 -0700

    Including output for demonstration

--- test-druid.php
+++ test-druid.php
@@ -12,9 +12,31 @@ $r = $c->executeQuery($q);
 
 var_dump( $r );
 
+//array(1) {
+//    [0]=>
+//  array(2) {
+//        ["timestamp"]=>
+//    string(24) "2011-06-01T00:00:11.000Z"
+//        ["result"]=>
+//    array(2) {
+//            ["minTime"]=>
+//      string(24) "2011-06-01T00:00:11.000Z"
+//            ["maxTime"]=>
+//      string(24) "2011-11-30T23:55:34.000Z"
+//    }
+//  }
+//}
+
+
 $q = new \DruidFamiliar\TransformingTimeBoundaryDruidQuery('referral-visit-test-data');
 
 $r = $c->executeQuery($q);
 
 var_dump( $r );
 
+//array(2) {
+//    ["minTime"]=>
+//  string(24) "2011-06-01T00:00:11.000Z"
+//    ["maxTime"]=>
+//  string(24) "2011-11-30T23:55:34.000Z"
+//}
