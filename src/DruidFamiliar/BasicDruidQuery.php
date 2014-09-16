8bd998b7acd148477885d5da274f57fa1d5a25f1
Jasmine Hegman <jasmine@webpt.com>
2014-09-16 14:14:18 -0700

    Re-ordering so abstract functions are on top

--- src/DruidFamiliar/BasicDruidQuery.php
+++ src/DruidFamiliar/BasicDruidQuery.php
@@ -5,6 +5,35 @@ namespace DruidFamiliar;
 abstract class BasicDruidQuery implements IDruidQuery
 {
 
+    /**
+     * Take parameters and return a valid Druid Query in PHP Associative Array form.
+     *
+     * @param array $params
+     * @return array Druid JSON POST Body in PHP Array form
+     * @throws Exception\MissingParametersException
+     */
+    abstract public function generateQuery();
+
+    /**
+     * Hook function to handle response from server.
+     *
+     * This hook must return the response, whether changed or not, so that the rest of the system can continue with it.
+     *
+     * @param array $response
+     * @return mixed
+     */
+    abstract public function handleResponse($response = Array());
+
+    /**
+     * @param array $params
+     * @return mixed|void
+     * @throws Exception\MissingParametersException
+     */
+    abstract public function validateParams(Array $params);
+
+
+
+
     protected $params = Array();
 
 
@@ -21,26 +50,6 @@ abstract class BasicDruidQuery implements IDruidQuery
 
     }
 
-    /**
-     * Take parameters and return a valid Druid Query in PHP Associative Array form.
-     *
-     * @param array $params
-     * @return array Druid JSON POST Body in PHP Array form
-     * @throws Exception\MissingParametersException
-     */
-    abstract public function generateQuery();
-
-
-    /**
-     * Hook function to handle response from server.
-     *
-     * This hook must return the response, whether changed or not, so that the rest of the system can continue with it.
-     *
-     * @param array $response
-     * @return mixed
-     */
-    abstract public function handleResponse($response = Array());
-
 
     /**
      * Get the currently configured query params
@@ -53,13 +62,6 @@ abstract class BasicDruidQuery implements IDruidQuery
 
         return $this->params;
     }
-
-    /**
-     * @param array $params
-     * @return mixed|void
-     * @throws Exception\MissingParametersException
-     */
-    abstract public function validateParams(Array $params);
 
     /**
      * Set the query params
